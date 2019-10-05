5ae83fc8475eda35f32881e000511f1ac24a6cb6
jnmist <131308+jnmist@users.noreply.github.com>
2019-10-04 19:23:30 -0700

    take user input in startup.sh

--- startup.sh
+++ startup.sh
@@ -7,8 +7,12 @@ php artisan key:generate
 echo "Envoy.blade.php" >> .gitignore
 git init
 git status
-git config user.name jmist
-git config user.email j@localhost
+echo "GIT CONFIG USER.NAME - type in name to use:"
+read var1
+git config user.name $var1
+echo "GIT CONFIG USER.EMAIL - type in email to use:"
+read var2
+git config user.email $var2
 git add -A .
 git commit -m "initial commit"
 echo "TODO ... git remote add / and push to your repo ... "
