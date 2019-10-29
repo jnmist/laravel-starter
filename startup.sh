746e2c591babb5bdd3c094f2bb30f870b95f3b28
jnmist <131308+jnmist@users.noreply.github.com>
2019-10-28 21:15:17 -0700

    composer update, add debugbar, and tweak startup.sh

--- startup.sh
+++ startup.sh
@@ -7,13 +7,12 @@ php artisan key:generate
 echo "Envoy.blade.php" >> .gitignore
 git init
 git status
-echo "GIT CONFIG USER.NAME - type in name to use:"
-read var1
-git config user.name $var1
-echo "GIT CONFIG USER.EMAIL - type in email to use:"
-read var2
-git config user.email $var2
-git add -A .
-git commit -m "initial commit"
-echo "TODO ... git remote add / and push to your repo ... "
 echo "Deleting startup.sh"
+echo " "
+echo "All done!"
+echo " "
+echo "-------- YOU TODO ---------------"
+echo "1. Edit .env file with your settings"
+echo "2. add git remote"
+echo "3. stage and commit changes & and push to your repo ... "
+
